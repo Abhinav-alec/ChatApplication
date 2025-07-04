6c9932b6cf798c7b1526fc55fe3b1bdf8dd88251
Abhinav kumar jha <abhinav234765@gmail.com>
2025-07-05 00:15:43 +0530

    Login_backend

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -39,3 +39,30 @@ export const signup = async (req, res) => {
     });
   }
 };
+
+// Controller to login a user
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+    if (!isPasswordCorrect) {
+      return res.json({ seccess: false, message: "Invalid Credentials" });
+    }
+    const token = generateToken(newData._id);
+    res.json({
+      success: true,
+      userData,
+      token,
+      message: "Login successful",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
