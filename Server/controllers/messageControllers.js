3d529d4d78273533e00d670c4e94d9e11b3bb503
Abhinav kumar jha <abhinav234765@gmail.com>
2025-07-06 02:37:29 +0530

    userForSidebar

---
+++ Server/controllers/messageControllers.js
@@ -0,0 +1,29 @@
+import Message from "../models/Message";
+
+//get all users except the logged in user
+export const getUserForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
+
+    //Count number of messages not seen
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._Id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await Promise.all(promises);
+    res.json({ success: true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
